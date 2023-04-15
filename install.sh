c8c798c33ced4870fa669d5378f87903f5292282
abplayzGH <95254250+abplayzGH@users.noreply.github.com>
2023-04-14 19:30:28 -0500

    install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo su
+sudo mkdir /compose
+sudo cd /compose
+sudo apt update && sudo apt install runc containerd docker.io docker-compose git -y
+sudo wget https://raw.githubusercontent.com/abplayzGH/webtop/main/docker-compose.yaml
+sudo docker-compose up -d
