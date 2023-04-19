96fe9cdf21d92995a9a698ebf648f9e7e8c293a8
Adam <95254250+abplayzGH@users.noreply.github.com>
2023-04-19 10:50:53 -0500

    Create webtop-start.sh

---
+++ webtop-start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+BEGIN INIT INFO
+Provides: webtop-start.sh
+Required-Start: $all
+Required-Stop: $all
+Default-Start: 2 3 4 5
+Default-Stop: 0 1 6
+Short-Description: Starts webtop container at boot time
+END INIT INFO
+
+sudo docker-compose -f /compose/docker-compose.yaml up
+
