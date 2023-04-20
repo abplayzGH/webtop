5d53c025fa9eafcd7886ca4ee2d3952d15442ebf
Adam <95254250+abplayzGH@users.noreply.github.com>
2023-04-20 07:59:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ sudo mkdir /compose
 sudo cd /compose
 sudo apt update && sudo apt install runc containerd docker.io docker-compose git -y
 sudo wget -P /compose/ https://raw.githubusercontent.com/abplayzGH/webtop/main/docker-compose.yaml
-sudo docker-compose -f /compose/docker-compose.yaml/ up
+sudo docker-compose -f /compose/docker-compose.yaml up
 sudo wget -P /etc/init.d https://raw.githubusercontent.com/abplayzGH/webtop/main/webtop-start.sh
